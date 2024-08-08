eda8b574ad1b3da5cf2a0a0637001768b40d5f88
Reihan Agam <reihanagam7@gmail.com>
2024-08-08 23:47:13 +0700

    update

--- app/Views/order/index.php
+++ app/Views/order/index.php
@@ -17,11 +17,11 @@
             <img class="w-24 h-24 rounded-lg object-cover mr-4" src="https://images.tokopedia.net/img/KRMmCm/2022/9/23/74c8ec25-e12c-4743-96a4-2a3dd264a7c6.jpg" alt="Ikan Segar">
             <div class="flex-1">
                 <h3 class="text-lg font-semibold">Ikan</h3>
-                <!-- <div class="mt-2">
+                <div class="mt-2">
                   <button aria-label="Decrease quantity" class="text-gray-500 hover:text-gray-700 mr-2">-</button>
                     <span class="text-gray-700">1</span>
                   <button aria-label="Increase quantity" class="text-gray-500 hover:text-gray-700 ml-2">+</button>
-                </div> -->
+                </div>
             </div>
             <div class="text-lg font-semibold"><?= formatRupiah(10000) ?></div>
             <!-- <button class="text-red-500 hover:text-red-700 ml-4">Remove</button> -->
